5edb71b50494b7dfd2afd38337df1665602ada9b
Rafal <rafalpacholek@gmail.com>
2020-04-19 19:48:01 +0200

    Add dynamic routing

--- app/src/index.js
+++ app/src/index.js
@@ -5,12 +5,17 @@ import 'react-input-range/lib/css/index.css'
 import Games from './list.js';
 import MyGames from './add.js';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      website: 0,
       token: "",
     };
 
@@ -54,28 +59,44 @@ class Menu extends React.Component {
     if (this.state.token !== "") {
     return (
     <div>
-    <nav className="navbar is-link" role="navigation" aria-label="main navigation">
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item is-spaced {this.state.website == 0 && 'is-active'}" 
-              onClick={()=>this.setState({website:0})}>
-            List
-          </a>
-          <a className="navbar-item is-spaced {this.state.website == 1 && 'is-active'}"
-              onClick={()=>this.setState({website:1})}>
-            MyGames
-          </a>
-          <a className="navbar-item is-spaced" onClick={()=>this.setState({website:2})}>
-            Meeting
-          </a>
+      <Router>
+       <div>
+          <nav className="navbar is-link" role="navigation" aria-label="main navigation">
+            <div className="navbar-menu">
+              <div className="navbar-start">
+                <Link to="/" className="navbar-item is-spaced" > 
+                  List
+                </Link>
+                <Link to="/my-games" className="navbar-item is-spaced">
+                  MyGames
+                </Link>
+                <Link to="/meeting" className="navbar-item is-spaced">
+                  Meeting
+                </Link>
+              </div>
+              <div className="navbar-end">
+                <Link to="/profile" className="navbar-item is-spaced">
+                  Profile
+                </Link>
+              </div>
+            </div>
+          </nav>
         </div>
-      </div>
-    </nav>
-    <div>
-      {this.state.website == 0 && <Games token={this.state.token} />}
-      {this.state.website == 1 && <MyGames token={this.state.token} />}
-      {this.state.website == 2 && <h1>Meeting</h1>}
-    </div>
+        <Switch>
+          <Route exact path="/">
+            <Games token={this.state.token} />
+          </Route>
+          <Route path="/my-games">
+            <MyGames token={this.state.token} />
+          </Route>
+          <Route path="/meeting">
+            <h1>Meeting</h1>
+          </Route>
+          <Route path="/profile">
+            <h1>Profile</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
     )
     } else {
